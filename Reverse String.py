bd97ee1cc383242b01306a4792282244cb65dc0c
ParkCheung <1025711995@qq.com>
2016-06-05 17:09:16 +0800

    Reverse String.py

---
+++ Reverse String.py
@@ -0,0 +1,12 @@
+class Solution():
+
+    def reverseString(self, s):
+        """
+        :type x: str
+        :rtype: str
+        """
+        return s[::-1]
+
+#debug
+s = Solution()
+print(s.reverseString("abn"))
