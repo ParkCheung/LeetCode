1f8e9f3cda14a9ed1066bab0a0d194210ecc1ee5
ParkCheung <1025711995@qq.com>
2016-06-05 17:00:46 +0800

    Reverse Integer.py

---
+++ Reverse Integer.py
@@ -0,0 +1,19 @@
+class Solution():
+
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 0:
+            sign = -1
+            x = -x
+        else:
+            sign = 1
+        strx = str(abs(x))
+        r = int(strx[::-1])
+
+        return sign * r if r < (1<<31)-1 else 0
+#debug
+s = Solution()
+print(s.reverse(-2147483641))
