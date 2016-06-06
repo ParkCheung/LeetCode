d4350801f47d2d2b117d0ec795aa5ea9a36b9083
ParkCheung <1025711995@qq.com>
2016-06-06 21:38:04 +0800

    Binary Tree

---
+++ Invert Binary Tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root is None:
+            return root
+            
+        if root.left != None:
+        	self.invertTree(root.left)
+       	if root.right != None:
+       		self.invertTree(root.right)
+
+       	if root.left != root.right:
+       		root.left,root.right = root.right,root.left
+
+       	return root;
+
---
+++ Maximum Depth of Binary Tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+#递归计算左子书和右子树的深度 DFS
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        else:
+            return max(self.maxDepth(root.left),self.maxDepth(root.right))+1
+#debug
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+
+s = Solution()
+print(s.maxDepth(root))
