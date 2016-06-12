962879ca8b136aa2db40beecc3bfce6b7dce40d5
ParkCheung <1025711995@qq.com>
2016-06-12 22:28:18 +0800

    Contains Duplicate.py

---
+++ Contains Duplicate.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(nums) != len(set(nums))
+#debug
+
+s = Solution()
+print(s.containsDuplicate([2,1]))
